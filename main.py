3bab8cecc5e3d15b347bb5c0202c4d0ce847c4a4
szykol <szymek554@gmail.com>
2019-01-08 01:07:37 +0100

    - usprawnienie wykrywania i rozdzielania monet

--- main.py
+++ main.py
@@ -9,16 +9,23 @@ def display_img(im, title='default', cmap='gray'):
 
 #%%
 from skimage import io
+import skimage
+import scipy
 import cv2
 
 # załadowanie oryginalnego obrazka
 # test_image = io.imread('test_rozdzielnie.jpg')
+#test_image = io.imread('img/Obraz (11).jpg')
+
 test_image = io.imread('test_lacznie.jpg')
 
-display_img(test_image)
+test_image_small = scipy.misc.imresize(test_image, 0.2)
+
+display_img(test_image_small)
 
 # przeksztalcenie obrazka do skali szarosci
-grey = cv2.cvtColor(test_image, cv2.COLOR_RGB2GRAY)
+shifted = cv2.pyrMeanShiftFiltering(test_image_small, 21, 51)
+grey = cv2.cvtColor(shifted, cv2.COLOR_RGB2GRAY)
 display_img(grey, 'W odcieniach szarosci')
 
 # binaryzacja obrazka
@@ -28,49 +35,37 @@ display_img(test_bin, 'Binarny')
 
 #%%
 import numpy as np
-kernel = np.ones((10, 10),np.uint8)
-# kernel = np.ones((3,3), np.uint8)
-# erosion = cv2.erode(test_bin,kernel2,iterations = 3)
-closing = cv2.morphologyEx(test_bin, cv2.MORPH_CLOSE, kernel, iterations=4)
-cont_img = closing.copy()
-
+kernel = np.ones((3, 3),np.uint8)
+closing = cv2.morphologyEx(test_bin, cv2.MORPH_CLOSE, kernel, iterations=1)
 display_img(closing)
-_, contours, hierarchy = cv2.findContours(cont_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-print(len(contours))
-
-for cnt in contours:
-    area = cv2.contourArea(cnt)
-    # if area < 2000 or area > 4000:
-    #     continue
-    if len(cnt) < 5:
-        continue
-    ellipse = cv2.fitEllipse(cnt)
-    # cv2.ellipse(test_image, ellipse, (0,255,0), 2)
-    cv2.ellipse(test_image, ellipse, (0,255,0), 2, cv2.LINE_AA)
-
-# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-# closing = cv2.morphologyEx(test_bin,cv2.MORPH_OPEN,kernel)
-
-display_img(test_image, 'Test')
 
 #%%
-# noise removal
-kernel = np.ones((3,3),np.uint8)
-# opening = cv2.morphologyEx(test_bin,cv2.MORPH_OPEN,kernel, iterations = 2)
-opening = closing.copy()
-# sure background area
-sure_bg = cv2.dilate(opening,kernel,iterations=3)
+
+sure_bg = cv2.dilate(closing,kernel,iterations=3)
 # Finding sure foreground area
-dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+dist_transform = cv2.distanceTransform(closing,cv2.DIST_L2,3)
 ret, sure_fg = cv2.threshold(dist_transform,0.5*dist_transform.max(),255,0)
+
 # Finding unknown region
 sure_fg = np.uint8(sure_fg)
 unknown = cv2.subtract(sure_bg,sure_fg)
+
+ret, markers = cv2.connectedComponents(sure_fg)
+
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers+1
+
+# Now, mark the region of unknown with zero
+markers[unknown==255] = 0
+
+markers = cv2.watershed(test_image_small,markers)
+test_image_small[markers == -1] = [0,255,0]
+
 display_img(dist_transform, 'Dist transform')
 display_img(sure_fg, 'Sure fg')
+display_img(test_image_small)
 
-
+#%%
 from scipy import ndimage as ndi
 label_objects, nb_labels = ndi.label(sure_fg)
 
@@ -79,7 +74,6 @@ mask_sizes = sizes > 20
 mask_sizes[0] = 0
 
 figures = mask_sizes[label_objects]
-
 
 #%%
 plt.imshow(figures, cmap="hot")
