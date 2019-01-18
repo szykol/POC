4dcbed33523c2fbab35784aeef078fa2d649559b
szykol <szymek554@gmail.com>
2019-01-18 13:50:51 +0100

    - rozpoczecie pracy nad obsluga obrazkow innych niz kwadratowe

--- segmentation.py
+++ segmentation.py
@@ -18,15 +18,12 @@ current_index = 0
 
 
 im = cv2.imread('ksztalty.png')
-im = cv2.resize(im, (512,512)) 
+im = cv2.resize(im, (512,256)) 
 grey = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
 th, bin = cv2.threshold(grey,0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-cv2.imshow('cos', bin)
 
 def splitting(image):
-    cv2.imshow('cos', image)
     indices = image.copy()
-    
     
     split(indices)     
     merge(image, indices)
@@ -43,25 +40,31 @@ def splitting(image):
 
 def split(chunk, level=0):
     global current_index
-    size = len(chunk)
-    a = int(size / 2)
+    try:
+        size_x = len(chunk[0])
+    except IndexError:
+        current_index += 1
+        return current_index
+    size_y = len(chunk)
+    a = int(size_x / 2)
+    b = int(size_y / 2)
     temp = True
-    for i in range(len(chunk[0])):
-        for j in range(len(chunk)):
+    for i in range(size_y):
+        for j in range(size_x):
             if(chunk[i,j] != chunk[0,0]):
                 temp = False
                 break
     
     if not temp:
         f = partial(split, level=level+1)
-        chunk[:a,:a] = f(chunk[:a,:a])
-        chunk[:a, a:size] = f(chunk[:a, a:size])
-        chunk[a:size, a:size] = f(chunk[a:size, a:size])
-        chunk[a:size, :a] = f(chunk[a:size, :a])
+        chunk[:b,:a] = f(chunk[:b,:a])
+        chunk[:b, a:size_x] = f(chunk[:b, a:size_x])
+        chunk[b:size_y, a:size_x] = f(chunk[b:size_y, a:size_x])
+        chunk[b:size_y, :a] = f(chunk[b:size_y, :a])
         return chunk
     else:
         current_index += 1
-        return np.full((size, size), current_index)
+        return np.full((size_y, size_x), current_index)
 
 def merge(image, indices):
     LUT = np.zeros_like(image, bool)
@@ -98,7 +101,7 @@ def merge(image, indices):
                 except IndexError:
                     pass
 
-            LUT[x,y] = True
+            LUT[y,x] = True
 
     # print(indices)
 
@@ -124,19 +127,10 @@ def clear_indices(indices):
 def color_objects(image, indices):
     max_index = np.amax(indices)
     # colored = np.zeros_like(image, dtype=tuple)
-    colored = np.zeros((len(indices[0]),len(indices),3), np.uint8)
+    colored = np.zeros((len(indices), len(indices[0]),3), np.uint8)
     colored[indices == 0] = (0, 0, 0)
     for i in range(1, max_index+1):
         colored[indices == i] = (13 * i, 25 * i, 30 * (max_index + 1 - i))
-
-
-
-        # for x in range(len(colored[0])):
-        #     for y in range(len(colored)):
-        #         if indices[x,y] == i:
-        #             colored[x,y] = (13 * i, 25 * i, 30 * (max_index + 1 - i))
-        #         elif indices[x,y] == 0:
-        #             colored[x,y] = (0,0,0)
     
     return colored
 
